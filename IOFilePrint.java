c608704f06325bbbc4b2ee74d154de0fe5f91ecf
fotisPag <fotis.pagonis01@gmail.com>
2024-04-16 22:00:31 +0300

    project IOPrintFile

---
+++ IOFilePrint.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.ch9;
+
+/*
+ * Εκτυπώνει σε αρχείο χαρακτήρες με τη χρήση
+ * των PrinStream και PrintWriter
+ *
+ * */
+
+
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class IOFilePrint {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintStream ps = new PrintStream("C:/Users/Fotis/Videos/OPA/Video- 9/IOSimpleTest1.txt");
+        PrintWriter pw = new PrintWriter("C:/Users/Fotis/Videos/OPA/Video- 9/IOSimpleTest2.txt");
+
+        ps.println("Hello world from Printstream");
+        pw.println("Hello world from PrintWriter");
+
+
+        ps.close();
+        pw.close();
+    }
+
+}
